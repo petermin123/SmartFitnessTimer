a8b4482796c6dbea58e7ae2bea835ccb9c433e0b
Honggang (Peter) Min <honggang.peter.min@gmail.com>
2023-07-12 20:36:54 -0400

    Isolated the data needed for cell
    Set up correct interactions between the 2 VCs as well as improving the UI a bit. Isolated the right information that will be needed to populate to each cell in the TableViewController.

--- SmartFitnessTimer/MainAlarmsViewController.swift
+++ SmartFitnessTimer/MainAlarmsViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class MainAlarmsViewController: UITableViewController {
     
+    let itemArray = ["1", "2", "3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.navigationItem.hidesBackButton = true
     }
 
     @IBAction func addNewAlarmButton(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "createNewAlarm", sender: self)
+    }
+    
+    //MARK: TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItem", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 
 }
--- SmartFitnessTimer/NewAlarmViewController.swift
+++ SmartFitnessTimer/NewAlarmViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class NewAlarmViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        alarmPicker.delegate = self
+        alarmPicker.dataSource = self
+    }
+    
     let pickerData: [[Int]] = [
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -19,7 +26,12 @@ class NewAlarmViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     ]
     
     @IBOutlet weak var alarmPicker: UIPickerView!
+    @IBOutlet weak var alarmName: UITextField!
+    var selectedMinute: Int = 0
+    var selectedSecond: Int = 0
+    var selectedAlarmName: String = ""
     
+    //MARK: PickerView DataSource and Delegate Methods
     // Desired number of columns of pickers
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
@@ -32,19 +44,14 @@ class NewAlarmViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     
     // Captures the picker view selection
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("User selected \(pickerData[component][row]) at component number \(component)")
+        selectedMinute = pickerView.selectedRow(inComponent: 0)
+        selectedSecond = pickerView.selectedRow(inComponent: 1)
+        selectedAlarmName = alarmName.text!
     }
     
     // Displays the data passed in as the title for the picker view
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return String(pickerData[component][row])
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        alarmPicker.delegate = self
-        alarmPicker.dataSource = self
-    }
-    
+
 }
